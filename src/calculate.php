80a07114853e5aafc8768f735a8edc00915d9c0e
ato210830 <ayato.083021@gmail.com>
2025-08-03 12:09:46 +0900

    #2 計算アプリの不具合を修正

--- src/calculate.php
+++ src/calculate.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once 'src/Calculator.php';
+require_once 'class/calculator.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['num1']) && isset($_POST['num2']) && isset($_POST['operation'])) {
